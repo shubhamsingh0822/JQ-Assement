01859efdbc49d6f302d73415491d1ae5eb13fee8
shubhamsingh0822 <146051065+shubhamsingh0822@users.noreply.github.com>
2023-12-29 13:06:35 +0530

    Add files via upload

--- J.Q Assesment/form.js
+++ J.Q Assesment/form.js
@@ -1,19 +1,36 @@
 
 
 function pass() {
+
+    // ########################################################## password input section ################################################
+
+
     var pas = document.getElementById("input").value
+
+    // ################################################################ for week password ###################################################
     
     if(pas.length<=5){
        var erro =document.getElementById("error").innerHTML="Weak"
        document.getElementById("error").style.color="red"
+       document.getElementById("error").style.="white"
 
     }
+
+    // ################################################################ for normal password ###################################################
+
+
     else if (pas.length>5 && pas.length<=8) {
         var erro =document.getElementById("error").innerHTML="Normal"
         document.getElementById("error").style.color="blue  "
     }
+
+    // ################################################################ for strong password ###################################################
+   
+    
     else{
         var erro =document.getElementById("error").innerHTML="Good"
         document.getElementById("error").style.color="green"
+        document.getElementById("error").style.backgroundColor="azure"
+
     }
 }
